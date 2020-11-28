2081facbe9cd233dc24662f79c2ea3644d027470
FatemaFouzia <fatemaakter12427@gmail.com>
2020-11-28 23:13:56 +0600

    Initial commit

---
+++ src/HshMapDemo.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+import java.util.Map;
+public class HshMapDemo {
+        public static void main(String args[])
+        {
+            Map<String, Integer> hm = new HashMap<String, Integer>();
+
+            hm.put("a", 100);
+            hm.put("b", 200);
+            hm.put("c", 300);
+            hm.put("d", 400);
+            System.out.println(hm.entrySet());
+            System.out.println(hm);
+            // Traversing through the map
+
+            for (Map.Entry<String, Integer> me : hm.entrySet()) {
+                System.out.print(me.getKey() + ":");
+                System.out.println(me.getValue());
+                
+            }
+        }
+}
